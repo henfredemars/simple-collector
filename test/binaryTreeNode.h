1efe359e312e69b7e2cd742de6cdab363b267bdd
henfredemars <marsrover7@fastmail.com>
2015-12-19 01:24:58 -0500

    Reverted modifications after studying collector limitations

--- test/binaryTreeNode.h
+++ test/binaryTreeNode.h
@@ -1,13 +1,14 @@
 
 #include <unordered_set>
 #include "../src/include/gc_obj.h"
+#include "../src/include/collector.h"
 
 class BinaryTreeNode : public gc_obj {
 public:
 	BinaryTreeNode(const int id);
 	int size() const;
 	void curtailToLevel(const int lvl);
-	void extendToLevel(const int size);
+	void extendToLevel(const int size,Collector* collector);
 	void addLeftChild(BinaryTreeNode* leftChild);
 	void addRightChild(BinaryTreeNode* rightChild);
 	virtual void finalize();
